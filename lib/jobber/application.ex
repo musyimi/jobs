1527189a12612632a1abee76f36ca12baeb46712
musyimi <musyimi13@gmail.com>
2023-01-07 16:16:22 +0300

    adjusted the restart frequency at application.ex

--- lib/jobber/application.ex
+++ lib/jobber/application.ex
@@ -7,8 +7,13 @@ defmodule Jobber.Application do
 
   @impl true
   def start(_type, _args) do
+    job_runner_config = [
+      strategy: :one_for_one,
+      max_seconds: 30,
+      name: Jobber.JobRunner
+    ]
     children = [
-      {DynamicSupervisor, strategy: :one_for_one, name: Jobber.JobRunner},
+      {DynamicSupervisor, job_runner_config},
       # Starts a worker by calling: Jobber.Worker.start_link(arg)
       # {Jobber.Worker, arg}
     ]
